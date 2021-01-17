f00923c633501f81b1572fc5dcb651db753d6677
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-01-17 18:59:04 -0300

    Initial Commit

---
+++ iOS-seeWeather-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-seeWeather-SwiftUI
+//
+//  Created by João Gabriel Dourado Cervo on 17/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-seeWeather-SwiftUI/iOS_seeWeather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_seeWeather_SwiftUIApp.swift
+//  iOS-seeWeather-SwiftUI
+//
+//  Created by João Gabriel Dourado Cervo on 17/01/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_seeWeather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
